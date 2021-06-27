11596cbbd1ec706e4804da54a5f62ba3b04a5842
Melissa Silva <32807254+melipass@users.noreply.github.com>
2021-06-27 18:57:12 -0400

    Flask application logging.

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,7 +1,10 @@
 from flask import Flask
 from flask import Response
+import logging
+from datetime import datetime
 
 app = Flask(__name__)
+logging.basicConfig(filename = 'app.log', level = logging.DEBUG)
 
 @app.route("/")
 def hello():
@@ -9,10 +12,12 @@ def hello():
 
 @app.route("/status")
 def status():
+    app.logger.debug('{}, status request successfull'.format(datetime.now()))
     return Response('{"result":"OK - Healthy"}', status=200, mimetype='application/json')
 
 @app.route("/metrics")
 def metrics():
+    app.logger.debug('{}, metrics request successfull'.format(datetime.now()))
     return Response('{"UserCount":140,"UserCountActive":23}', status=200, mimetype='application/json')
 
 if __name__ == "__main__":
