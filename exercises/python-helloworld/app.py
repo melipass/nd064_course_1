7807682d61e1e2edf418c12128f4fdd705c531d1
Melissa Silva <32807254+melipass@users.noreply.github.com>
2021-06-27 18:33:45 -0400

    New routes with status and metrics responses.

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,19 @@
 from flask import Flask
+from flask import Response
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "<h1>Hello World!</h1>"
+
+@app.route("/status")
+def status():
+    return Response('{"result":"OK - Healthy"}', status=200, mimetype='application/json')
+
+@app.route("/metrics")
+def metrics():
+    return Response('{"UserCount":140,"UserCountActive":23}', status=200, mimetype='application/json')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
